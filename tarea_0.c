56dcfb02c250ae0813394cf1e694e3a68ac87ce4
Carmen <hidalgo.paz.carmen@gmail.com>
2025-02-24 19:46:04 -0600

    Botones y circulo
    Ya se ve en Glade el circulo y los botones de barajar y terminar. El de terminar es el único que funciona ahorita

--- tarea_0.c
+++ tarea_0.c
@@ -2,40 +2,63 @@
 #include <cairo.h>
 #include <math.h>
 
-// Callback for drawing on a drawing area
-static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
-    // Set color and draw a circle
+gboolean dibujar_circulo(GtkWidget *area, cairo_t *cr, gpointer user_data) {
+    gint width = gtk_widget_get_allocated_width(area);
+    gint height = gtk_widget_get_allocated_height(area);
+    // Draw a circle centered in the area, filling it as much as possible
     cairo_set_source_rgb(cr, 0.6, 0.6, 0.6);
-    cairo_arc(cr, 100, 100, 50, 0, 2 * M_PI);
+    cairo_arc(cr, width / 2.0, height / 2.0, MIN(width, height) / 2.0, 0, 2 * M_PI);
     cairo_fill(cr);
     return FALSE;
 }
 
+// Para que no se mueva la línea del panel
+void fijar_panel(GtkPaned *panel, GParamSpec *pspec, gpointer user_data) {
+    const int pos_fijada = 895;    // Posición donde se fija la división
+    int current_pos = gtk_paned_get_position(panel);
+    if (current_pos != pos_fijada) {
+        gtk_paned_set_position(panel, pos_fijada);
+    }
+}
+
 int main(int argc, char *argv[]) {
-    GtkBuilder *builder;
-    GtkWidget *window;
-    GtkWidget *drawing_area;
+    GtkBuilder *builder;        // Utilizado para obtener los objetos de glade
+    GtkWidget *ventana;         // La ventana
+    GtkWidget *area_circulo;    // El área donde se dibuja el círculo
+    GtkWidget *panel;           // El panel que divide el área de dibujo y el área de interacción
+    GtkWidget *boton_salida;    // Botón para terminar el programa
+    GtkWidget *boton_barajar;   // Botón para barajar los datos
 
     gtk_init(&argc, &argv);
 
-    // Load UI from Glade file
+    // Cargar la interfaz de Glade
     builder = gtk_builder_new_from_file("interfaz.glade");
 
-    // Get the main window from the Glade file
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    // La ventana
+    ventana = GTK_WIDGET(gtk_builder_get_object(builder, "ventana"));
+    g_signal_connect(ventana, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Get the drawing area from the Glade file
-    drawing_area = GTK_WIDGET(gtk_builder_get_object(builder, "drawing_area"));
-    g_signal_connect(drawing_area, "draw", G_CALLBACK(on_draw_event), NULL);
+    // El panel divisor
+    panel = GTK_WIDGET(gtk_builder_get_object(builder, "division"));
+    g_signal_connect(panel, "notify::position", G_CALLBACK(fijar_panel), NULL);
 
-    // Show the window
-    gtk_widget_show_all(window);
+    // El área de dibujo
+    area_circulo = GTK_WIDGET(gtk_builder_get_object(builder, "area_circulo"));
+    g_signal_connect(area_circulo, "draw", G_CALLBACK(dibujar_circulo), NULL);
 
-    // Run the GTK main loop
+    // El bóton de terminación del programa
+    boton_salida = GTK_WIDGET(gtk_builder_get_object(builder, "boton_terminar"));
+    g_signal_connect(boton_salida, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
+    // Mostrar ventana
+    gtk_widget_show_all(ventana);
+    // Que la ventana utilize toda la pantalla
+    gtk_window_fullscreen(GTK_WINDOW(ventana));
+
+    // Correr GTK
     gtk_main();
 
-    // Clean up
+    // Limpiar la memoria
     g_object_unref(builder);
 
     return 0;
